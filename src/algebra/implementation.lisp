570f3b1cb69691cd9d8f907b720e7108307e584a
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-01-24 14:43:08 +0100

    Added register-operator function.

--- src/algebra/implementation.lisp
+++ src/algebra/implementation.lisp
@@ -4,3 +4,7 @@
 (defmethod operatorp ((algebra standard-algebra) name)
   (and (symbolp name)
        (~>> algebra operators (gethash name) not null)))
+
+
+(defmethod register-operator ((algebra standard-algebra) (name symbol))
+  (setf (~>> algebra operators (gethash name)) name))

24313e0e0eb01dfc065488080e875f880ed4387a
Marek Kochanowlcz <mkochanowicz@ownedoutcomes.com>
2019-01-24 17:12:29 +0100

    Reformatted code.

--- src/evaluation/implementation.lisp
+++ src/evaluation/implementation.lisp
@@ -15,13 +15,13 @@
                                 state)
   (case (cl-autograd.graph:form-type form)
     (list
-     (cl-autograd.algebra:evaluate-operator-value
-      algebra
-      (~> form
-          cl-autograd.graph:content
-          first)
-      form
-      state))
+     (~> form
+         cl-autograd.graph:content
+         first
+         (cl-autograd.algebra:evaluate-operator-value algebra
+                                                      _
+                                                      form
+                                                      state)))
     (number
      (setf (~>> form cl-autograd.graph:index (cl-autograd.tape:value-at state))
            (cl-autograd.graph:content form)))))
